353dd75f5191f0bdfbe24314aaa09701f1a14914
Antonio <35410603+redyz13@users.noreply.github.com>
2022-11-02 18:02:46 +0100

    new file:   shell_args.c

---
+++ SO/10_22/shell_args.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define BUFSIZE 64
+#define NARGS 128
+
+int main(void) {
+    pid_t pid;
+    char *list[NARGS];
+    char buff[BUFSIZE];
+    int ch, k;
+
+    do {
+        printf("%% ");
+        k = 0;
+
+        do {
+            list[k] = malloc(BUFSIZE);
+            scanf("%s", buff);
+
+            ch = getchar();
+
+            if (ch == ' ')
+                strcpy(list[k++], buff);
+            else if (ch == '\n') {
+                strcpy(list[k++], buff);
+                list[k] = NULL;
+                break;
+            }
+
+        } while (1);
+
+        if (strcmp(list[0], "exit") == 0) {
+            for (int i = 0; i < k; i++)
+                free(list[i]);
+            exit(0);
+        }
+
+        pid = fork();
+
+        if (pid < 0)
+            exit(-1);
+        else if (pid == 0) {
+            if (execvp(list[0], list) == -1) {
+                printf("Command \'%s\' not found\n", list[0]);
+                exit(-1);
+            }
+        }
+
+        wait(NULL);
+
+        for (int i = 0; i < k; i++)
+            free(list[i]);
+
+    } while (1);
+
+    return 0;
+}
