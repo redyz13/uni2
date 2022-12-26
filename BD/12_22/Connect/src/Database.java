532facf5817f67b6bb7245f7913ed350b365f8b6
Antonio <35410603+redyz13@users.noreply.github.com>
2022-12-26 18:15:39 +0100

    modified:   Connect/src/Database.java

--- BD/12_22/Connect/src/Database.java
+++ BD/12_22/Connect/src/Database.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class Database {
     private Connection connection;
-    private String databaseName, url, username, password;
+    private final String databaseName, username, password;
 
     public Database(String databaseName, String username, String password) {
         this.databaseName = databaseName;
@@ -12,13 +12,31 @@ public class Database {
 
     public void connect() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        url = "jdbc:mysql://localhost:3306/" + databaseName;
+        String url = "jdbc:mysql://localhost:3306/" + databaseName;
         connection = DriverManager.getConnection(url, username, password);
+    }
+
+    public void close() throws SQLException {
+        connection.close();
+    }
+
+    public void query() throws SQLException {
+        Statement statement;
+        ResultSet resultSet;
+
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery("SELECT * FROM Cliente");
+
+        for (int i = 0; resultSet.next(); i++)
+            System.out.println(i + " " + resultSet.getString("email"));
+
+        statement.close();
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Database database = new Database("ddl_progetto", "root", args[0]);
         database.connect();
-        System.out.println("Worked");
+        database.query();
+        database.close();
     }
 }
