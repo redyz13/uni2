acb05f5317213d3ca90b8d1d9981851d37de7025
Antonio <35410603+redyz13@users.noreply.github.com>
2022-12-27 21:22:53 +0100

    modified:   Suicide/src/InterfacciaQuery.java

--- BD/12_22/Suicide/src/Debug.java
+++ BD/12_22/Suicide/src/Debug.java
@@ -1,12 +1,26 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.Year;
 import java.util.Calendar;
 import java.sql.Date;
 
 public class Debug {
-    public static void main(String[] args) {
-        Calendar c = Calendar.getInstance();
-        c.setTime(new Date(System.currentTimeMillis()));
-        c.add(Calendar.YEAR, 5);
-        Date d = new Date(c.getTimeInMillis());
-        System.out.println(d);
+    public static void main(String[] args) throws SQLException {
+        int year = Year.now().getValue();
+        System.out.println(year);
+
+        Database database = new Database("ddl_progetto", "root", "Giggino@G1");
+        Query queryTest = new Query(database);
+        int StrutturaRicettiva;
+
+        Statement statement;
+        ResultSet resultSet;
+
+        statement = database.getConnection().createStatement();
+        resultSet = statement.executeQuery("SELECT MAX(codiceStruttura) FROM StrutturaRicettiva");
+        StrutturaRicettiva = resultSet.getInt("codiceStruttura") + 1;
+        statement.close();
+        System.out.println(StrutturaRicettiva);
     }
 }
