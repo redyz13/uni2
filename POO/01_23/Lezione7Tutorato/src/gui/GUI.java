80b6dff7328e18786c057a3c5dc87ddeb0983668
Antonio <35410603+redyz13@users.noreply.github.com>
2023-01-26 21:11:24 +0100

    modified:   src/gui/GUI.java

--- POO/01_23/Lezione7Tutorato/src/gui/GUI.java
+++ POO/01_23/Lezione7Tutorato/src/gui/GUI.java
@@ -12,6 +12,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GUI extends JFrame {
     private ArrayList<Player> playerList;
@@ -56,6 +57,39 @@ public class GUI extends JFrame {
             }
         });
         JButton simula = new JButton("Combattimento");
+        simula.addActionListener(e -> {
+            if (playerList == null) {
+                System.out.println("Lista non ancora creata");
+                return;
+            }
+            if (playerList.size() == 1) {
+                System.out.println(playerList.get(0).getClass().getName() + " vince il torneo!");
+            }
+            System.out.println("Simulazione combattimento tra: ");
+            Random random = new Random();
+
+            Player p1 = playerList.remove(random.nextInt(playerList.size()));
+            Player p2 = playerList.remove(random.nextInt(playerList.size()));
+            System.out.println(p1.getClass().getName() + " e " + p2.getClass().getName());
+
+            for (int i = 0; i <= 10; i++) {
+                if (i % 2 == 0)
+                    combattimento(p1, p2);
+                else
+                    combattimento(p2, p1);
+
+                if (p1.getPuntiVita() <= 0) {
+                    System.out.println("Player " + p2 + " vince!");
+                    playerList.add(p2);
+                    return;
+                }
+                if (p2.getPuntiVita() <= 0) {
+                    System.out.println("Player " + p1 + " vince!");
+                    playerList.add(p1);
+                    return;
+                }
+            }
+        });
 
         buttonPanel.add(crea);
         buttonPanel.add(salva);
@@ -64,5 +98,16 @@ public class GUI extends JFrame {
         mainPanel.add(buttonPanel);
 
         return mainPanel;
+    }
+
+    private void combattimento(Player p1, Player p2) {
+        System.out.println("Attacca " + p1.getClass().getName());
+        if (p1.attacco(p2))
+            System.out.println("Attacco riuscito!");
+        else
+            System.out.println("Attacco non riuscito!");
+        p1.potenziamento();
+        System.out.println(p1);
+        System.out.println(p2);
     }
 }
